55a8a7e357d01bc76e2ac0ac787b7373a83caa6d
yulius k wijaya <70045649+yulius98@users.noreply.github.com>
2025-06-14 12:00:25 +0700

    commit3

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 
 class ChatController extends Controller
 {
@@ -12,10 +14,51 @@ class ChatController extends Controller
     public function __invoke(Request $request)
     {
         $request->validate([
+            'sesion_id' => 'required',
             'content' => 'required',
         ]);
 
-        $res = Http::post('https://api.openai.com/v1/chat/completions', [
-            'headers' => [
+        $existingMessage = DB::table('chats')
+            ->where('session_id', $request->post('session_id'))
+            ->get();
+
+        $sessionId = $request->post('session_id');
+
+        $messages = [];
+        
+
+        if (count($existingChat) < 1) {
+            // If session exists, append new message
+            $messages = [
+                ['role' => 'system', 'content' => 'Kamu adalah asisten AI yang membantu pengguna dengan pertanyaan mereka.'],
+                ['role' => 'user', 'content' => $request->post('content')],
+            ];
+            $messageDb = array_map(function($data) use ($sessionId) {
+                $data['session_id'] = $sessionId;
+                return $data;
+            }, $messages);
+            DB::table('chats')->insert($messages);
+        } else {
+            $messages = $existingMessage->map(function ($data) {
+                return [
+                    'role' => $data->role,
+                    'content' => $data->content,
+                ];
+            })
+        }    
+
+        
+
+        
+
+        $res = Http::withToken('aaf6ae6aaab2494d85b62c7745783c96')
+            ->post('https://api.deepseek.com/chat/completions', [
+                'model' => 'deepseek-chat',
+                'messages' =>  $messages,
+                'stream' => false,
+                ]);
+        
+        $content = $res->json('choices.{first}.message.content');
+        
     }
 }
